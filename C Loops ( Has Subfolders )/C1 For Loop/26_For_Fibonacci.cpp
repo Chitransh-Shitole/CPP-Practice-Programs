512e74c4701f20686859453d8c25da6394db28f8
Chitransh-Shitole <chitranshshitole@gmail.com>
2026-07-30 18:48:35 +0530

    Program for Fibonacci Sequence using for loop.
    This commit adds a Simple Fibonacci Sequence Program using For Loop. ( Might be Tweaked Later )

---
+++ C Loops ( Has Subfolders )/C1 For Loop/26_For_Fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//THIS PROGRAM PRINTS FIBONACCI SERIES UPTO GIVEN TERMS USING FOR LOOP
+int main ()
+{
+    int n, a = 0, b = 1, i, sum = 0 ;
+
+    cout << "This Program prints Fibonacci series using For Loop" << endl ;
+    cout << "Enter the Number ( Series limit ) : " ;
+    cin >> n ;
+
+    //LOOP IMPLEMENTATION
+    for ( i = 0 ; i < n ; i++ )
+    {
+        //COUT FIRST BECAUSE SEQUENCE STARTS FROM 0 ( SUM = 0 )
+        cout << sum << "\t" ;
+
+        //FIBONACCI SERIES LOGIC
+        sum = a + b ; 
+        a = b ;
+        b = sum ;
+    }
+    return 0 ; 
+}
